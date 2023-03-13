6b5784f8419aa80d0d03a24ddf9fb86c7cc1d294
swapnith <swapnithkondapalli@gmail.com>
2023-03-13 23:55:36 +0530

    chnages

--- land/src/App.js
+++ land/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container-fluid">
+      <Login/>
     </div>
   );
 }
---
+++ land/src/components/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24"/>
+            </a>
+        </div>
+       </nav>
+    </div>
+  )
+}
+
+export default Login
