7df69c7767da6b2a755faf334daf0ab743fbb96c
peeravali0110 <peeravali0110@gmail.com>
2023-03-14 10:28:24 +0530

    required warning for email

--- land/src/components/Home/index.js
+++ land/src/components/Home/index.js
@@ -21,7 +21,7 @@ function Home() {
         <form>
             <div className="mb-3">
                 <label for="exampleInputEmail1" className="form-label text-white fsc-bold">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                <input type="email" required className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
                 <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
             </div>
             <div className="mb-3">
